aa710b7de71048e129de2bd57745110d2cd08733
Edwar Martinez Vale <edwar_kenedy@hotmail.com>
2024-04-20 10:47:04 -0400

    scaffold UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/6d12d8b1-336b-48fb-8a4b-d3762050f1e7-1x9aim.jpeg",
+  "https://utfs.io/f/23fa48ff-65a7-4dcc-bea0-00ce6a7673a0-1x9ahw.jpeg",
+  "https://utfs.io/f/71d0f89f-184b-44d2-903a-12bea83da510-1x9ai0.jpeg",
+  "https://utfs.io/f/32e4a8fa-f8a0-4e6d-a953-96c22b584c51-1x9ahy.jpeg",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (gallery in progress)
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map(
+          (image) => (
+            <div key={image.id} className="w-48">
+              <img src={image.url} />
+            </div>
+          ),
+        )}
+      </div>
     </main>
   );
 }
